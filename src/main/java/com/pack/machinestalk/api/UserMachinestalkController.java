1cc92f5601ffe549dcf949c64422b05093ed4bef
rabiihlioui <rabiihlioui@gmail.com>
2022-12-29 00:20:58 +0100

    second commit

--- src/main/java/com/pack/machinestalk/api/UserMachinestalkController.java
+++ src/main/java/com/pack/machinestalk/api/UserMachinestalkController.java
@@ -2,8 +2,7 @@ package com.pack.machinestalk.api;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.pack.machinestalk.model.UserMachinestalk;
-import com.pack.machinestalk.repo.UserMachinestalkRepo;
-import com.pack.machinestalk.service.UserMachinestalkService;
+import com.pack.machinestalk.service.UserMachinestalkServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,16 +10,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.Collection;
+import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class UserMachinestalkController {
     @Autowired
-    private UserMachinestalkService userMachinestalkService;
+    private UserMachinestalkServiceImpl userMachinestalkServiceImpl;
 
     @GetMapping("/home")
     public String home() {
@@ -29,16 +29,19 @@ public class UserMachinestalkController {
 
     @GetMapping("/users")
     public ResponseEntity<List<UserMachinestalk>> getUsers() throws JsonProcessingException {
-        return ResponseEntity.ok(userMachinestalkService.findAllUsers());
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/users").toUriString());
+        return ResponseEntity.created(uri).body(userMachinestalkServiceImpl.findAllUsers());
     }
 
     @GetMapping("/users/gender/{type}")
     public ResponseEntity<List<UserMachinestalk>> getUsersByGender(@PathVariable String type) throws JsonProcessingException {
-        return ResponseEntity.ok(userMachinestalkService.findByGender(type));
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/users/gender/" + type).toUriString());
+        return ResponseEntity.created(uri).body(userMachinestalkServiceImpl.findByGender(type));
     }
 
     @GetMapping("/users/status/{type}")
     public ResponseEntity<List<UserMachinestalk>> getUsersByStatus(@PathVariable String type) throws JsonProcessingException {
-        return ResponseEntity.ok(userMachinestalkService.findByStatus(type));
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/users/status/" + type).toUriString());
+        return ResponseEntity.created(uri).body(userMachinestalkServiceImpl.findByStatus(type));
     }
 }
--- src/main/java/com/pack/machinestalk/service/UserMachinestalkService.java
+++ src/main/java/com/pack/machinestalk/service/UserMachinestalkService.java
@@ -1,62 +1,12 @@
 package com.pack.machinestalk.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pack.machinestalk.model.UserMachinestalk;
-import com.pack.machinestalk.repo.UserMachinestalkRepo;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
 import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-public class UserMachinestalkService {
-    /*@Autowired
-    private RestTemplate template = new RestTemplate();*/
-
-    @Autowired
-    private UserMachinestalkRepo usersRepo;
-
-    public List<UserMachinestalk> findAllUsers() throws JsonProcessingException {
-        String uri = "https://gorest.co.in/public-api/users";
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-        String jsonString = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class).getBody();
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode rootNode = objectMapper.readTree(jsonString);
-        JsonNode idNode = rootNode.path("data");
-        System.out.println("testttt " + jsonString);
-        System.out.println("idNode " + idNode);
-        ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
-
-        System.out.println("###########################################");
-        System.out.println("Printing GET Response in String");
-        System.out.println("###########################################");
-
-        System.out.println(result.getBody());
-        List<UserMachinestalk> studentList = objectMapper.readValue(idNode.toString(), new TypeReference<List<UserMachinestalk>>() {});
-        System.out.println("studentList " + studentList);
-        usersRepo.saveAll(studentList);
-        return studentList;
-        //Object[] objs = restTemplate.getForObject("https://gorest.co.in/public-api/users", Object[].class);
-        //System.out.println("Objs " + Arrays.asList(objs));
-        //return Arrays.asList(objs);
-    }
-
-    public List<UserMachinestalk> findByGender(String gender) {
-        return usersRepo.findByGender(gender);
-    }
-
-    public List<UserMachinestalk> findByStatus(String type) {
-        return usersRepo.findByStatus(type);
-    }
+public interface UserMachinestalkService {
+    List<UserMachinestalk> findAllUsers() throws JsonProcessingException;
+    List<UserMachinestalk> findByGender(String type);
+    List<UserMachinestalk> findByStatus(String type);
 }
